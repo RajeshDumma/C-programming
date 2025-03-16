721ff0e5cd3aa204528018fb32b126d52b9074bc
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-16 11:00:27 +0530

    DataTypes in c

---
+++ output/Basic-in-c-language/dataTypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+  // there are three data types in c language
+  /* 1. primary data Types
+     2. Derived Data types
+     3. user-Defined Data Types
+  */
+  // primary data types ==> int , float , double , char , void
+ //  and more like , short int , long int , long double etc..
+    return 0;
+}
