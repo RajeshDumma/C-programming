00fbbfb122afd0bd0ee826dc1e27b3aafab81c52
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-16 10:32:31 +0530

    printFormate 's

---
+++ output/printFormate.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+ printf("c "); 
+ printf("Programming ");
+ printf("Language "); // all print statements will print in same line 
+ 
+  printf("\n print in new line \n");
+  printf("c \n"); 
+ printf("Programming \n");
+ printf("Language \n");
+
+ printf("I am Learning\n C program \n from \n youTube");
+
+ return 0;
+}
