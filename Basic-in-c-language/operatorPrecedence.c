5fad699463e7d591fd3de1d8ede9b2aa18e6dfa2
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-16 16:48:51 +0530

    operator Procedence in c

--- Basic-in-c-language/operatorPrecedence.c
+++ Basic-in-c-language/operatorPrecedence.c
@@ -4,9 +4,9 @@
 int main (){
     int result;
     // 1st expesion
- //   result = 4 + 4 * 4 - 4 / 4 ;
+    result = 4 + 4 * 4 - 4 / 4 ;
    //  result = 3 + 3 * 3 - 3 / 3 ;
-     result = (3 + 3) * 3 - 3 / 3 ;
+   //  result = (3 + 3) * 3 - 3 / 3 ;
     printf("%d",result);
     return 0;
 }
