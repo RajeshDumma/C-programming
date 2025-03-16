789d65065b41e4675c5301cd0390eeb954449a81
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-16 16:06:59 +0530

    Operator precedence in c (bodmas)

---
+++ Basic-in-c-language/operatorPrecedence.c
@@ -0,0 +1,12 @@
+// print the result of expresion 3 + 3 * 3 - 3 / 3
+
+#include <stdio.h>
+int main (){
+    int result;
+    // 1st expesion
+ //   result = 4 + 4 * 4 - 4 / 4 ;
+   //  result = 3 + 3 * 3 - 3 / 3 ;
+     result = (3 + 3) * 3 - 3 / 3 ;
+    printf("%d",result);
+    return 0;
+}
