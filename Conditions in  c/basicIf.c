4d5656424c18e2e2735af6ebacdbbfcc01742843
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-17 10:09:11 +0530

    if-else condition

---
+++ Conditions in  c/basicIf.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a = 10;
+    if(a >= 11){
+        printf("i am learning c language"); // here only if condidtion not else part
+    } 
+        printf("i am learning c++ language");
+    return 0;
+}
+
+// finding greatest number
+#include <stdio.h>
+
+int main(){
+    
+    int a = 10;
+    int b = 20;
+    if(a > b){
+        printf("a is grater then b");
+    } else{
+        printf("b is greater then a");
+    }
+    return 0;
+}
+
+  // collect age finding the major and minor of person
+
+ #include <stdio.h>
+int main(){
+    int age;
+    scanf("%d",&age);
+    if(age >= 18){
+        printf("You are a major");
+    } else{
+        printf("you are a minor");
+    }
+    return 0;
+}
+
