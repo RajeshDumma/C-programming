ce324f57f8a26d6c8a0b2d5bf9f489503fc55058
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-17 15:36:38 +0530

    do-While loop in c

---
+++ Loops in C/doWhileLoop.c
@@ -0,0 +1,12 @@
+// Do while execuition count is 1
+
+#include <stdio.h>
+
+int main(){
+    int a = 10;
+    do{
+        printf("Hey wecome!");
+    }while(a > 12); 
+// here condition is failed but , it will execuit one time
+    return 0;
+}
