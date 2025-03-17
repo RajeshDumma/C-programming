3f6be2471cd34e25ea6969ec0d7cfa82685ff951
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-17 15:16:49 +0530

    While Loop

---
+++ Loops in C/whileLoop.c
@@ -0,0 +1,19 @@
+// while loop in c language 
+
+#include <stdio.h>
+
+// basic loop 
+
+int main(){
+    int a = 10 ;
+    while(a >= 1){ // condition is true , so loop can execuit
+        printf("condition is true %d \n",a);
+        a--;
+    }
+
+   /*  while(a <= 11){ // condition false
+        printf("condition is true %d \n",a);
+        a--;
+    } */
+    return 0;
+}
