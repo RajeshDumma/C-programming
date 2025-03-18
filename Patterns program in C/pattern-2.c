74b4bfcf71330312e244feaa54a3cd114ade95f7
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-18 21:35:05 +0530

    commit changed

--- Patterns program in C/pattern-2.c
+++ Patterns program in C/pattern-2.c
@@ -22,6 +22,8 @@
     for(int i = 1 ; i <= n ;i++){
         for(int j = 1 ; j <= i ; j++){
             printf("* ");
+          //  printf("%d ",i); // it's print row wise 
+            //  printf("%d ",j); //it's print column wise 
         }
         printf("\n");
     }
