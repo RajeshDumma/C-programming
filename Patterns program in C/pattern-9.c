ee1d55a459559a2cdf1821d1367e6d0921215dc6
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-18 23:34:11 +0530

    changes commits in pattren 9

--- Patterns program in C/pattern-9.c
+++ Patterns program in C/pattern-9.c
@@ -1,15 +1,3 @@
-// pattern 1 
-/*
-  1 2 3 4 5  
-1 * * * * *
-2 * * * * *
-3 * * * * *
-4 * * * * *
-5 * * * * *
-
-// here 5 rows and 5 column
- */
- // fist check number of rows and cols
 
  #include <stdio.h>
 
@@ -32,17 +20,3 @@
     return 0;
  }
 
- // pattern 1 
-/*
-  1 2 3 4 5  
-1 * * * * *
-2 * * * * *
-3 * * * * *
-4 * * * * *
-5 * * * * *
-
-// here 5 rows and 5 column
- */
- // fist check number of rows and cols
-
- 
