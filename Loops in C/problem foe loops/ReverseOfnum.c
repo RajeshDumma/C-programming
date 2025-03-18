f266614a805252c162b236c0bfbd1c7afe25f387
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-18 20:25:26 +0530

    Reverse of a number

---
+++ Loops in C/problem foe loops/ReverseOfnum.c
@@ -0,0 +1,20 @@
+// Reverse of a number in 
+
+#include <stdio.h>
+
+
+int main(){
+
+     int num , reminder , reverse = 0;
+     printf("Enter a number : ");
+     scanf("%d",&num);
+
+     while(num != 0){
+        reminder = num % 10 ;
+        reverse = reverse * 10 + reminder;
+        num = num / 10;
+     }
+     printf("%d",reverse);
+
+    return 0;
+}
