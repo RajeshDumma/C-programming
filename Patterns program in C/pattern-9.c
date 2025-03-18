7e512ba22a110cce278038bee4f27a352f62ad27
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-18 23:30:38 +0530

    pattern-9

---
+++ Patterns program in C/pattern-9.c
@@ -0,0 +1,33 @@
+// pattern 1 
+/*
+  1 2 3 4 5  
+1 * * * * *
+2 * * * * *
+3 * * * * *
+4 * * * * *
+5 * * * * *
+
+// here 5 rows and 5 column
+ */
+ // fist check number of rows and cols
+
+ #include <stdio.h>
+
+ int main(){
+   
+    int n , rows , cols , space;
+    printf("Enter n value : ");
+    scanf("%d",&n);
+
+    for(rows = 1 ;rows <= n ;rows++){
+       for(space = 1; space <= n - rows ; space++){
+        printf("  ");
+       }
+        for(cols = 1 ;cols <= rows ;cols++){
+            printf("%d ",n-cols + 1);
+        }
+        printf("\n");
+    }
+
+    return 0;
+ }
