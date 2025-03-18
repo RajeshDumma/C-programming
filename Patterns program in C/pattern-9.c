ecec3fd93a88c6950a3f2566d22d8770172d1d3e
RajeshDumma <157523264+RajeshDumma@users.noreply.github.com>
2025-03-18 23:34:55 +0530

    Update pattern-9.c

--- Patterns program in C/pattern-9.c
+++ Patterns program in C/pattern-9.c
@@ -19,4 +19,27 @@
 
     return 0;
  }
+==========================================================================================
 
+ for Albhabet code
+
+#include <stdio.h>
+
+ int main(){
+   
+    int n , rows , cols , space;
+    printf("Enter n value : ");
+    scanf("%d",&n);
+
+    for(rows = 1 ;rows <= n ;rows++){
+       for(space = 1; space <= n - rows ; space++){
+        printf("  ");
+       }
+        for(cols = 1 ;cols <= rows ;cols++){
+            printf("%c ",rows + 64);
+        }
+        printf("\n");
+    }
+
+    return 0;
+ }
