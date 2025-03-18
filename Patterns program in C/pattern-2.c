6c2881ff5b293312e360e910ab246a92d60181da
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-18 21:12:48 +0530

    pattern-2

---
+++ Patterns program in C/pattern-2.c
@@ -0,0 +1,30 @@
+// pattern 1 
+/*
+  1 2 3 4 5  
+1 *
+2 * * 
+3 * * *
+4 * * * *
+5 * * * * *
+
+// here 5 rows and 5 column
+ */
+ // fist check number of rows and cols
+
+ #include <stdio.h>
+
+ int main(){
+   
+    int n ;
+    printf("Enter n value : ");
+    scanf("%d",&n);
+
+    for(int i = 1 ; i <= n ;i++){
+        for(int j = 1 ; j <= i ; j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+ }
