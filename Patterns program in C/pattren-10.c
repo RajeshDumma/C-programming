98066daef910819c0bd85bd136b2b5f38b18d452
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-18 23:45:15 +0530

    pattren 10

---
+++ Patterns program in C/pattren-10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+ int main(){
+   
+    int n , rows , cols , space;
+    printf("Enter n value : ");
+    scanf("%d",&n);
+
+    for(rows = 1 ;rows <= n ;rows++){
+       for(space = 1; space <= rows - 1 ; space++){
+        printf("  ");
+       }
+        for(cols = 1 ;cols <= n - rows + 1 ;cols++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+ }
