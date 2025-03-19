570948832e14659c7c806775d45d1d9aafe8785e
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-19 20:45:36 +0530

    pattren -15

---
+++ Patterns program in C/pattren-15.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n, rows, cols, space;
+
+    printf("Enter n value: ");
+    scanf("%d", &n);
+
+    // Upper part (increasing stars)
+    for (rows = 1; rows <= n; rows++) {
+        for (space = 1; space <= n - rows; space++) {
+            printf(" ");
+        }
+        for (cols = 1; cols <= rows; cols++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    // Lower part (decreasing stars)
+    for (rows = 1; rows < n; rows++) {
+        for (space = 1; space <= rows; space++) {
+            printf(" ");
+        }
+        for (cols = 1; cols <= n - rows; cols++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+ 
+
+
