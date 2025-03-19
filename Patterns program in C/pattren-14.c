fbf1fd6534ed9c0919e2d820cc4d417acf406029
RajeshDumma <157523264+RajeshDumma@users.noreply.github.com>
2025-03-19 15:37:19 +0530

    Update pattren-14.c

--- Patterns program in C/pattren-14.c
+++ Patterns program in C/pattren-14.c
@@ -30,3 +30,33 @@ int main() {
 
     return 0;
 }
+
+
+// another method with in 3 for loops 
+
+#include <stdio.h>
+
+int main() {
+    int n, rows, cols, space ,stars;
+
+    printf("Enter n value: ");
+    scanf("%d", &n);
+
+    // Single loop for both increasing and decreasing stars
+    for (rows = 1; rows <= 2 * n - 1; rows++) {
+         stars = (rows <= n) ? rows : (2 * n - rows); // Control star count
+        // Printing spaces
+        for (space = 1; space <= n - stars; space++) {
+            printf(" ");
+        }
+        // Printing stars
+        for (cols = 1; cols <= stars; cols++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+
