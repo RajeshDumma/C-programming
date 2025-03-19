dc98a98583ad59078c54ae9ed1a426316356c26d
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-19 14:35:36 +0530

    pattren-11

--- Patterns program in C/pattern-11.c
+++ Patterns program in C/pattern-11.c
@@ -26,4 +26,31 @@ int main(){
 * * * * *
 */
 
+// rows 
+
+#include <stdio.h>
+
+int main(){
+
+
+    int n , rows , cols , space ;
+    printf("Enter the number : ");
+    scanf("%d",&n);
+    for(rows = 1; rows <= n; rows++){
+        for(space = 1; space <= n - rows; space++){
+       printf(" ");
+        } 
+        for(cols = 1 ; cols <= rows ; cols++){
+            printf("%d ",cols);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+/* // output :-
+    1 
+   2 2 
+  3 3 3 
+ 4 4 4 4 
+5 5 5 5 5  */
 
