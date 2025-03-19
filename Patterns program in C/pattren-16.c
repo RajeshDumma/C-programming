c1ff8ffee94716b6bee398a7e4802d40251318ea
RajeshDumma <157523264+RajeshDumma@users.noreply.github.com>
2025-03-19 21:01:53 +0530

    Update pattren-16.c

--- Patterns program in C/pattren-16.c
+++ Patterns program in C/pattren-16.c
@@ -19,9 +19,42 @@ int main() {
 
     return 0;
 } */
+
+    *
+   ***
+  *****
+ *******
+*********
  
 // same as reverse pattren 
 
+#include <stdio.h>
 
+int main() {
+    int n, rows, cols, space;
 
+    printf("Enter n value: ");
+    scanf("%d", &n);
+
+    // Upper part (increasing stars)
+    for (rows = n; rows >= 1; rows--) {
+        for (space = 1; space <= n - rows; space++) {
+            printf(" ");
+        }
+        for (cols = 1; cols <= rows *2-1; cols++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+// 
+
+*********
+ *******
+  *****
+   ***
+    *
 
