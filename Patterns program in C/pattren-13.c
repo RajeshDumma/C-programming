aba097810665e5e4d7bd8d8c6934aa6e907863ba
RajeshDumma <157523264+RajeshDumma@users.noreply.github.com>
2025-03-19 14:52:55 +0530

    Update pattren-13.c

--- Patterns program in C/pattren-13.c
+++ Patterns program in C/pattren-13.c
@@ -21,3 +21,40 @@
 
     return 0;
  }
+  ---------------------------------------------------------------------------------------------
+// another logic 
+
+#include <stdio.h>
+
+ int main(){
+   
+    int n , rows , cols , space;
+    printf("Enter n value : ");
+    scanf("%d",&n);
+
+    for(rows = 1 ;rows <= n ;rows++){
+        for(cols = 1 ;cols <= rows ;cols++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+    for(rows = 1 ; rows < n;rows++){
+        for(cols = 1; cols <= n - rows; cols++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+ }
+// output 
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
