34052fe382335e67d23028e44dcc83cb33ab6514
RajeshDumma <157523264+RajeshDumma@users.noreply.github.com>
2025-03-20 05:31:00 +0530

    Update pattern-17.c

--- Patterns program in C/pattern-17.c
+++ Patterns program in C/pattern-17.c
@@ -29,8 +29,68 @@ int main() {
     return 0
 } 
  
+// output :
+ * * * * * 
+  * * * * 
+   * * * 
+    * * 
+     * 
+    * * 
+   * * * 
+  * * * * 
+ * * * * * 
+
+// same method for numbers
+
+#include <stdio.h>
+
+int main() {
+    int n, rows, cols, space;
+
+    printf("Enter n value: ");
+    scanf("%d", &n);
+
+    // Upper part (increasing stars)
+    for (rows = 1; rows <= n; rows++) {
+        for (space = 1; space <= rows ; space++) {
+            printf(" ");
+        }
+        for (cols = 1; cols <= n - rows + 1; cols++) {
+            printf("%d ",cols+rows-1);
+        }
+        printf("\n");
+    }
+    for (rows = 1; rows < n; rows++) {
+        for (space = 1; space <= n - rows ; space++) {
+            printf(" ");
+        }
+        for (cols = 1; cols <=  rows+1; cols++) {
+            printf("%d ",cols-rows+ n-1);
+        }
+        printf("\n");
+    }
+
+    return 0;
+} 
+ 
 
 
+
+
+
+
+
+// output:-
+
+ 1 2 3 4 5 
+  2 3 4 5 
+   3 4 5 
+    4 5 
+     5 
+    4 5 
+   3 4 5 
+  2 3 4 5 
+ 1 2 3 4 5 
 
 
 
