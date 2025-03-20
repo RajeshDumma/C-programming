f7aef3144d6c543905174c49d1f641a4a5bdc0c6
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-20 06:11:51 +0530

    pattren-19 in c

---
+++ Patterns program in C/pattren-19.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n, rows, cols, space ,colsLimit;
+
+    printf("Enter n value: ");
+    scanf("%d", &n);
+    
+    for (rows = 1; rows <= n; rows++) {
+        for (cols = 1; cols <= n; cols++) {
+            if(cols == 1 || cols == n || rows == 1 || rows == n )
+            printf("* ");
+             else 
+                printf("  ");
+        } 
+        printf("\n");
+    }
+return 0;
+}
