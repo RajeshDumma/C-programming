95fb4f73b7493e60594924b7d354a8b318f92b07
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-21 07:07:14 +0530

    categoty of functions

---
+++ Function in C/categoriesOfFn.c
@@ -0,0 +1,32 @@
+/* 
+ 4 types of catecories in c
+
+ 1. without Argument
+    without return value
+
+ 2.  without Argument
+     with return value
+     
+3. with argument ;
+   without return value   
+
+4. with Argument
+   with return value
+*/
+
+// 1st one 
+/* 
+#include <stdio.h>
+void greet();
+
+int main(){
+   
+    greet();
+    return 0;
+}
+ 
+void greet(){
+    printf("welcome to my World!");
+} */
+
+
