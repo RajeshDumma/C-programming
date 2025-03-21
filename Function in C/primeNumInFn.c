974476eb78ea8c2e93d32da9f017ae22fcd46473
RajeshDumma <157523264+RajeshDumma@users.noreply.github.com>
2025-03-21 20:42:44 +0530

    Update primeNumInFn.c

--- Function in C/primeNumInFn.c
+++ Function in C/primeNumInFn.c
@@ -31,4 +31,38 @@ int checkPrime(int a) {
 }
 
 
-// check upto n numbers 
+// check upto n numbers
+
+#include <stdio.h>
+
+int checkPrime(int a);  // Function prototype
+
+int main() {
+    int num;
+    
+    // Taking input from user
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    printf("Prime numbers up to %d are: ", num);
+    for (int i = 2; i <= num; i++) {
+        if (checkPrime(i)) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
+
+// Function to check if a number is prime
+int checkPrime(int a) {
+    if (a < 2) return 0;  // Numbers less than 2 are not prime
+
+    for (int j = 2; j * j <= a; j++) { // Optimized check up to sqrt(a)
+        if (a % j == 0) {
+            return 0;  // Not a prime number
+        }
+    }
+    return 1;  // Prime number
+}
+
