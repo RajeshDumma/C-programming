8ffcd7184725151ee56acde6ded8ea6965f72ac3
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-22 11:26:07 +0530

    sin , cos and tan in c

---
+++ libraryFunctions in c/sin_cos_tan.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+
+   double x;
+
+    printf("Enter the Number : ");
+    scanf("%lf",&x);
+
+    printf("%.8lf",sin(x));
+ 
+    return 0;
+}
+
+// cos  and tan
