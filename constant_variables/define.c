76cd9ae822ebfa9d3f58537766ebf763280f1857
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-22 19:30:25 +0530

    #define variables

---
+++ constant_variables/define.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define I 100
+#define C 'D'
+#define F 10.10
+#define D 20.34567
+#define MESSAGE "WELCOME TO MY WORLD!"
+
+int main(){
+ printf(" I = %d\n C = %c\n F = %.4f\n D = %.10lf\n MESSAGE = %s ",I , C, F , D , MESSAGE);
+
+    return 0;
+}
