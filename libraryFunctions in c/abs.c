243f6b3f73f755c23e669384e96b9ee4855a2a42
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-22 09:53:14 +0530

    absoluet value

---
+++ libraryFunctions in c/abs.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    int x ;
+    int absoluetValue;
+    printf("Enter a number : ");
+    scanf("%d",&x);
+
+   absoluetValue = abs(x);
+
+    printf("absoluetValue = %d" , absoluetValue);
+
+    return 0;
+}
+ 
+//  fabs library function 
+
+// it is used for floating points obsoluet
+
+
