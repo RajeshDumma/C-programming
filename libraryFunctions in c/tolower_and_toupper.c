2c8944cdafe14be230b4ffbf313bbe6773a01a9d
RajeshDumma <157523264+RajeshDumma@users.noreply.github.com>
2025-03-22 10:27:11 +0530

    Update tolower_and_toupper.c

--- libraryFunctions in c/tolower_and_toupper.c
+++ libraryFunctions in c/tolower_and_toupper.c
@@ -17,3 +17,22 @@ int main(){
 }
  
 // toupper case letter 
+
+
+#include<stdio.h>
+#include<ctype.h>
+
+int main(){
+
+    char ch;
+    char uppercase;
+
+    printf("Enter a lowercase letter : ");
+    scanf("%c",&ch);
+
+    uppercase = toupper(ch);
+
+    printf("%c",uppercase);
+
+    return 0;
+}
