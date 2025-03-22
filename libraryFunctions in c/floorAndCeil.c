56262449f5109d23f934ae839118da1414ade881
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-22 10:00:29 +0530

    floor And ceil problem

---
+++ libraryFunctions in c/floorAndCeil.c
@@ -0,0 +1,17 @@
+/* #include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double x ;
+    double floorValue;
+    printf("Enter a number : ");
+    scanf("%lf",&x);
+
+   floorValue = floor(x);
+
+    printf("floorValue = %.2lf" , floorValue);
+
+    return 0;
+} */
+// same as ceil value 
