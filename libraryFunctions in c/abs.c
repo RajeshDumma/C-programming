95644950a291a0c3a9893a299ecfd8d3db6886f6
RajeshDumma <157523264+RajeshDumma@users.noreply.github.com>
2025-03-22 09:55:12 +0530

    Update abs.c

--- libraryFunctions in c/abs.c
+++ libraryFunctions in c/abs.c
@@ -19,4 +19,20 @@ int main(){
 
 // it is used for floating points obsoluet
 
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double x ;
+    double absouletValue;
+    printf("Enter a number : ");
+    scanf("%lf",&x);
+
+   absouletValue = fabs(x);
+
+    printf("absouletValue = %.2lf" , absouletValue);
+
+    return 0;
+}
 
