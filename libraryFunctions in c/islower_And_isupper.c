3751bdfaedff6f3cba82eb21186a787d8312ecf3
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-22 10:16:49 +0530

    islower and isupper case in c

---
+++ libraryFunctions in c/islower_And_isupper.c
@@ -0,0 +1,38 @@
+/* 
+int islower(int c); // function declaration
+islower
+  tests if the given alphbaet is lowercase letter or not 
+ input : t
+ output : 2  // its will came 0 that is false
+
+// when we pass a lowercase letter , it may return any integer other then 0
+
+
+int isupper(int c)
+ //isupper 
+ tests if the given alphbaet is uppercase letter or not 
+ input : T
+ output : 0
+ // when we pass a uppercase letter , it may return any integer other then 0
+*/
+
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+
+    char ch ;
+  int check ;
+    printf("Enter a Alphabet : ");
+    scanf("%c",&ch);
+
+   check = islower(ch);
+
+    printf("check = %d" , check);
+
+    return 0;
+}
+ 
+  // isupper or not , if not it will return 0 , if yes return 1 or 2 something values
+ 
