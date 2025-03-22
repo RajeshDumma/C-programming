1d5f9332708854a9bd345a84d995c9ca7b94289c
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-22 10:39:39 +0530

    isAlpha and isDigit in c

---
+++ libraryFunctions in c/isAlpha.c
@@ -0,0 +1,26 @@
+//  int isalpha(int c); 65 to 90 && 97 to 122
+// return some values like 1 or 2 , if not return 0;
+
+/* 
+#include<stdio.h>
+#include<ctype.h>
+
+int main(){
+
+    char ch;
+    int isAlpha;
+
+    printf("Enter a Character : ");
+    scanf("%c",&ch);
+
+    isAlpha = isalpha(ch);
+
+    printf("%d",isAlpha);
+
+    return 0;
+} */
+
+
+// isdigit ==> it will check 48 to 57 in between this number
+// it's return 1 or 2 , if not it's return 0
+
