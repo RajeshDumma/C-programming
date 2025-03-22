ad84315ab3ba0e8f2340ed93d391ed55d8579637
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-22 10:26:42 +0530

    uppercase and lowercase in c

---
+++ libraryFunctions in c/tolower_and_toupper.c
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+#include<ctype.h>
+
+int main(){
+
+    char ch;
+    char lowercase;
+
+    printf("Enter a uppercase letter : ");
+    scanf("%c",&ch);
+
+    lowercase = tolower(ch);
+
+    printf("%c",uppercase);
+
+    return 0;
+}
+ 
+// toupper case letter 
