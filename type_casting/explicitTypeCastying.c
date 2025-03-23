77c26a3c1a15f9d1abd1c9ee0ac92851b1877fb3
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-03-23 07:15:38 +0530

    explicit type conversion

---
+++ type_casting/explicitTypeCastying.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 3;
+    
+    float c = a / b;  // Integer division occurs first: 10 / 3 = 3
+    printf("%f \n", c);  // Output: 3.000000 (Data loss)
+
+    int x = 10;
+    int y = 3;
+    
+    float z = (float)x / y;  // Explicit type casting ensures floating-point division (10.0/3) = 3.333
+    printf("%.2f \n", z);  // Output: 3.33(Correct result)
+  
+    float z = x / (float) y ;
+    
+    return 0;
+}
