fd09d2ef7263c43818576a043fd04ffc31d6cd35
RajeshDumma <157523264+RajeshDumma@users.noreply.github.com>
2025-04-15 22:26:29 +0530

    Update factorial.c

--- recurcive_Functions/factorial.c
+++ recurcive_Functions/factorial.c
@@ -20,3 +20,31 @@ int fact(int n) {
     else
         return 1;
 }
+
+---------------------------------------------------------------------
+
+    #include <stdio.h>
+
+int fact(int); // Function declaration
+
+int main() {
+    int num;
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        int result = fact(num);
+        printf("Factorial of %d is: %d\n", num, result);
+    }
+
+    return 0;
+}
+
+int fact(int n) {
+    if (n >= 1)
+        return n * fact(n - 1);
+    else
+        return 1;
+}
