594cb03f1fcc3919037bbb8091135bf46371113b
Rajesh7142 <rajeshdumma2004@gamil.com>
2025-05-01 07:12:15 +0530

    Addition and Subtraction in two D array

---
+++ Arrays in c/addOfTwoMatrix.c
@@ -0,0 +1,40 @@
+ #include<stdio.h>
+
+int main(){
+    int rows , cols;
+    printf("Enter the rows : ");
+    scanf("%d",&rows);
+    printf("Enter the cols : ");
+    scanf("%d",&cols);
+
+    int arrA[rows][cols] , arrB[rows][cols] , result[rows][cols] ;
+
+
+    if()
+    printf("Enter first matrix values : ");
+
+    for(int i = 0; i < rows ; i++){
+        for(int j = 0; j < cols ; j++){
+            scanf("%d",&arrA[i][j]);
+        }
+    }
+    printf("Enter second matrix values : ");
+    for(int i = 0 ; i < rows ; i++){
+        for(int j = 0; j< cols ; j++){
+            scanf("%d",&arrB[i][j]);
+        }
+    }
+    printf("Addition of two matrix is : \n");
+    for(int i = 0 ; i < rows ; i++){
+        for(int j = 0; j< cols ; j++){
+            result[i][j]= arrA[i][j] + arrB[i][j];
+            printf("%d ",result[i][j]); 
+        }
+        printf("\n");
+    }
+
+
+
+    return 0;
+} 
+
